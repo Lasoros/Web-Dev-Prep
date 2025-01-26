f5f716045089e481d100022af476c7c1e2833ca2
Lasoros <ipodtouch_guy@hotmail.com>
2025-01-26 15:30:20 -0500

    Added Questions For End Of Interview

--- src/questionsEndInt.tsx
+++ src/questionsEndInt.tsx
@@ -20,14 +20,14 @@ export const EndQuestions = () => {
 
 const items = [
     { value: "Employer Question 1", title: "What size team would I be workin with?"},
-    { value: "Employer Question 2", title: "Briefly Describe Your Experience"},
-    { value: "Employer Question 3", title: "How did you become interested in web development?"},
-    { value: "Employer Question 4", title: "Tell us what you learned recently."},
-    { value: "Employer Question 5", title: "What programming languages are you proficient in?"},
-    { value: "Employer Question 6", title: "Why Are You Interested in This Role?"},
+    { value: "Employer Question 2", title: "Will I have the opportunity to meet my team lead? (Unless already in the interview as suggested by research)"},
+    { value: "Employer Question 3", title: "Can you tell me about the team I'd be working with?"},
+    { value: "Employer Question 4", title: "In the interim of being onboarded, is there anything I should be going out of my way to familiarize myself with? (If it's going well)"},
+    { value: "Employer Question 5", title: "As far as first hiring entry level devs what is the most prominent challenge you have seen so I can do my best to avoid making the same pitfall"},
+    { value: "Employer Question 6", title: "Whats each of your favorite things about working for ReyRey?"},
     { value: "Employer Question 7", title: "What Are the Qualities Needed for The Success of This Role?"},
-    { value: "Employer Question 8", title: "What Are the Roles of an Entry Level Developer?"},
-    { value: "Employer Question 9", title: "Why Do You Want to Work for Our Organization?"},
+    { value: "Employer Question 8", title: "Would I be workin with any perpriority technology in this role?"},
+    { value: "Employer Question 9", title: "What are your expectations for this role during the first 30 to 60 days?"},
     { value: "Employer Question 10", title: "What do you do if you can’t work out a coding issue by yourself?"},
 ]
 
